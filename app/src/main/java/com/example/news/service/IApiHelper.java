b9b90d6174fb14e0ee6406552dd45e28a016ea86
arcolistro <arcolistro@gmail.com>
2020-07-15 10:10:21 -0700

    add constants, article class and apihelper

---
+++ app/src/main/java/com/example/news/service/IApiHelper.java
@@ -0,0 +1,19 @@
+package com.example.news.service;
+
+import com.example.news.model.News;
+import com.example.news.model.Source;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface IApiHelper {
+        //https://newsapi.org/v2/top-headlines?country=us&apiKey=98b0e7a42d3f4579a952c81ad267a17a
+        @GET("top-headlines")
+        Observable<News> getTopHeadlines(@Query("country") String country, @Query("apiKey") String apiKey);
+
+        //      Get Headlines from that source (bbc or nbc or nyt etc)
+//      https://newsapi.org/v2/top-headlines?sources=nbc-news&apiKey=98b0e7a42d3f4579a952c81ad267a17a
+        @GET("top_headlines")
+        Observable<Source> getSource(@Query("sources") String sources, @Query("apiKey") String apiKey);
+}
